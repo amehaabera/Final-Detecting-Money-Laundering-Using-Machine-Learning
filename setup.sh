e1b594439da8de12e0e5c05defc04d3f49d65714
Ameha Kidane <70386043+amehaabera@users.noreply.github.com>
2024-08-20 20:21:29 +0300

    setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,7 @@ echo "\
 email = \"amehaabera@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+[server]
+headless = true
+enableCORS = false
+port = 8501
